9cce20827afac56e45e48f0c7089660145dcc9af
John Sandler <priciel@protonmail.com>
2017-04-19 13:30:49 +0200

    Fix on tick callback

--- ForceTarget.lua
+++ ForceTarget.lua
@@ -1,30 +1,23 @@
 Menu = MenuElement{
-	name = "Force Target",
+	name = "Lines",
 	id = "_fc",
-	type = MENU,
-	leftIcon = "https://yt3.ggpht.com/-HGe-tYPCAy0/AAAAAAAAAAI/AAAAAAAAAAA/370X_ErZABE/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
+	type = MENU
 };
 
-local _target, va
-local __enem = {[0] = "None"};
-local __enemObj = {};
-do
-	for i = 1, Game.HeroCount() do
-		local hero = Game.Hero(i)
-		if not hero or hero.charName == "" then break end
-		if tonumber(hero.team) == 200 then
-			table.insert(__enem, hero.charName)
-			table.insert(__enemObj, hero)
-		end
-	end
-end
-
-Menu:MenuElement{ -- Menu._Force:Value()
-	name = "Set as Target",
-	id = "_Force",
-	value = 1,
-	drop = __enem,
-	callback = function(val) _target = __enemObj[val]; va = val end
+Menu:MenuElement{
+	name = "X",
+	id = "X",
+	value = 1, min = 0, max = 200, step = 1
+};
+Menu:MenuElement{
+	name = "Y",
+	id = "Y",
+	value = 1, min = 0, max = 200, step = 1
+};
+Menu:MenuElement{
+	name = "Z",
+	id = "Z",
+	value = 1, min = 0, max = 200, step = 1
 };
 
 
@@ -40,21 +33,11 @@ end
 
 local function Draws()
 	if _target ~= nil and _target.visible then
-		DrawLine3D(_target.pos.x + 45, _target.pos.y - 60, _target.pos.z, _target.pos.x + 45, _target.pos.y - 60, _target.pos.z + 160, 2, Draw.Color(255, 61, 249, 243))
-		DrawLine3D(_target.pos.x - 45, _target.pos.y - 60, _target.pos.z, _target.pos.x - 45, _target.pos.y - 60, _target.pos.z + 160, 2, Draw.Color(255, 61, 249, 243))
+		DrawLine3D(_target.pos.x + Menu.X:Value(), _target.pos.y - Menu.Y:Value(), _target.pos.z, _target.pos.x + Menu.X:Value(), _target.pos.y - Menu.Y:Value(), _target.pos.z + Menu.Z:Value(), 2, Draw.Color(255, 61, 249, 243))
+		DrawLine3D(_target.pos.x - Menu.X:Value(), _target.pos.y - Menu.Y:Value(), _target.pos.z, _target.pos.x - Menu.X:Value(), _target.pos.y - Menu.Y:Value(), _target.pos.z + Menu.Z:Value(), 2, Draw.Color(255, 61, 249, 243))
 		DrawText3D(_target.pos.x, _target.pos.y, _target.pos.z, Draw.Color(255, 253, 8, 44))
 	end
 end
 
-local function Ticks()
-	if _target == nil or __enem[va] == "None" then return end
-	if _G.SDK and _G.SDK.Orbwalker then
-		_G.SDK.Orbwalker.ForceTarget = _target
-	elseif _G.GOS then
-		GOS:ForceTarget(_target)
-	else
-	end
-end
-
 Callback.Add("Draw", function() Draws() end)
-Callback.Add("Draw", function() Ticks() end)
+Callback.Add("Tick", function() Ticks() end)
